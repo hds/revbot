8e972104ef685e348a7d5677ab4fc30820706199
Hayden Stainsby <hds@caffeineconcepts.com>
2021-09-07 17:40:40 +0200

    Added support for pipeline webhook
    The pipeline webhook can now also be deserialized. A message will be
    sent to the user defined in the webhook data structure when the pipeline
    is in one of the three states:
    * Running
    * Success
    * Failed
    
    Ideally the message about the pipeline would reference the MR it belongs
    to (in case it belongs to an MR), but there isn't enough data in the
    webhook to do this without further calls to the GitLab API.

--- src/main.rs
+++ src/main.rs
@@ -27,42 +27,77 @@ impl PartialEq for User {
     }
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 struct AssigneeChanges {
     current: Vec<User>,
     previous: Vec<User>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 struct Changes {
     assignees: Option<AssigneeChanges>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+enum MergeStatus {
+    Unchecked,
+    Checking,
+    CanBeMerged,
+    CannotBeMerged,
+    CannotBeMergedRecheck,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 struct MergeRequestAttributes {
     iid: u64,
-    merge_status: String,
+    merge_status: MergeStatus,
     title: String,
     url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+enum PipelineStatus {
+    Created,
+    WaitingForResource,
+    Preparing,
+    Pending,
+    Running,
+    Success,
+    Failed,
+    Canceled,
+    Skipped,
+    Manual,
+    Scheduled,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+struct PipelineAttributes {
+    finished_at: Option<String>,
+    id: u64,
+    #[serde(rename = "ref")]
+    ref_: String,
+    status: PipelineStatus,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 struct Project {
     name: String,
     web_url: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct WebhookMessage {
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+struct MergeRequestWebhook {
     assignees: Option<Vec<User>>,
     changes: Option<Changes>,
-    object_attributes: Option<MergeRequestAttributes>,
-    object_kind: String,
+    #[serde(rename = "object_attributes")]
+    merge_request: MergeRequestAttributes,
     project: Project,
     user: User,
 }
 
-impl WebhookMessage {
+impl MergeRequestWebhook {
     fn get_assignee_changes(&self) -> Option<&AssigneeChanges> {
         match &self.changes {
             Some(changes) => match &changes.assignees {
@@ -74,6 +109,23 @@ impl WebhookMessage {
     }
 }
 
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+struct PipelineWebhook {
+    #[serde(rename = "object_attributes")]
+    pipeline: PipelineAttributes,
+    project: Project,
+    user: User,
+}
+
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+#[serde(tag = "object_kind", rename_all = "snake_case")]
+enum Webhook {
+    MergeRequest(MergeRequestWebhook),
+    Pipeline(PipelineWebhook),
+}
+
 fn get_new_assignees(assignee_changes: &AssigneeChanges) -> Vec<User> {
     let current_assignees = &assignee_changes.current;
     current_assignees
@@ -83,26 +135,47 @@ fn get_new_assignees(assignee_changes: &AssigneeChanges) -> Vec<User> {
         .collect()
 }
 
-fn process_new_assignee(new_assignee: &User, webhook: &WebhookMessage) -> Option<webex::Message> {
-    let mr_attr = match &webhook.object_attributes {
-        Some(mr_attr) => mr_attr,
-        None => return None,
-    };
+fn process_new_assignee(new_assignee: &User, webhook: &MergeRequestWebhook) -> Option<webex::Message> {
+    let merge_request = &webhook.merge_request;
     let project = &webhook.project;
     let user = &webhook.user;
 
     let to_person_email = new_assignee.email.to_owned();
     let markdown = format!(
-        "(Test) [!{mr_iid} {mr_title}]({mr_url}) \
+        "MR [!{mr_iid} {mr_title}]({mr_url}) \
         ([{project_name}]({project_url})) \
         by @{user} \
         🤩 Added as assignee",
-        mr_iid=mr_attr.iid, mr_title=mr_attr.title, mr_url=mr_attr.url,
+        mr_iid=merge_request.iid, mr_title=merge_request.title, mr_url=merge_request.url,
         project_name=project.name, project_url=project.web_url, user=user.username);
 
     Some(webex::Message::new(
         to_person_email,
         markdown
+    ))
+}
+
+fn process_pipeline_status(webhook: &PipelineWebhook) -> Option<webex::Message> {
+    let pipeline = &webhook.pipeline;
+    let project = &webhook.project;
+    let user = &webhook.user;
+
+    let to_person_email = user.email.to_owned();
+    let status_text = match pipeline.status {
+        PipelineStatus::Success => Some("🌞 Success"),
+        PipelineStatus::Failed => Some("⛈️ Failed"),
+        PipelineStatus::Running => Some("⏳ Running"),
+        _ => None,
+    }?;
+    let markdown = format!(
+        "Pipeline [#{pipeline_id} {pipeline_ref}]({project_url}/-/pipelines/{pipeline_id}) \
+        {pipeline_status}",
+        pipeline_id=pipeline.id, pipeline_ref=pipeline.ref_, project_url=project.web_url,
+        pipeline_status=status_text);
+
+    Some(webex::Message::new(
+        to_person_email,
+        markdown,
     ))
 }
 
@@ -122,20 +195,29 @@ async fn handle(request: Request<Body>, webex_client: WebexClient) -> Result<Res
             *response.body_mut() = Body::from(my_string.to_uppercase());
             *response.status_mut() = StatusCode::OK;
 
-            let webhook_msg: Result<WebhookMessage, serde_json::Error> =
+            let webhook_msg: Result<Webhook, serde_json::Error> =
                 serde_json::from_str(&my_string);
             match webhook_msg {
-                Ok(webhook_msg) => {
-                    debug!("Web Hook: {:?}", webhook_msg);
-                    if let Some(assignee_changes) = webhook_msg.get_assignee_changes() {
+                Ok(Webhook::MergeRequest(webhook)) => {
+                    debug!("Merge Request Webhook: {:?}", webhook);
+                    if let Some(assignee_changes) = webhook.get_assignee_changes() {
                         for new_assignee in get_new_assignees(assignee_changes) {
-                            if let Some(msg) = process_new_assignee(&new_assignee, &webhook_msg) {
+                            if let Some(msg) = process_new_assignee(&new_assignee, &webhook) {
 
                                 match webex_client.clone().send_message(msg).await {
-                                    Ok(_) => info!("Sent message to: {}", &new_assignee.email),
-                                    Err(err) => warn!("Error sending message to {}: {:?}", &new_assignee.email, err),
+                                    Ok(_) => info!("Sent assignee message to: {}", &new_assignee.email),
+                                    Err(err) => warn!("Error sending assignee message to {}: {:?}", &new_assignee.email, err),
                                 }
                             }
+                        }
+                    }
+                }
+                Ok(Webhook::Pipeline(webhook)) => {
+                    debug!("Pipeline Webhook: {:?}", webhook);
+                    if let Some(msg) = process_pipeline_status(&webhook) {
+                        match webex_client.clone().send_message(msg).await {
+                            Ok(_) => info!("Sent pipeline message to: {}", &webhook.user.email),
+                            Err(err) => warn!("Error sending pipeline message to {}: {:?}", &webhook.user.email, err),
                         }
                     }
                 }
@@ -224,4 +306,115 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_deserialize_merge_request() {
+        let json = r#"
+      {
+      "object_attributes": {
+        "created_at": "2021-09-06 10:54:57 -0500",
+        "description": "",
+        "id": 289144,
+        "iid": 3,
+        "merge_error": null,
+        "merge_status": "unchecked",
+        "merge_when_pipeline_succeeds": false,
+        "state": "opened",
+        "state_id": 1,
+        "url": "https://main.gitlab.in.here.com/stainsby/mr-test/-/merge_requests/3",
+        "title": "Fail pipeline"
+      },
+      "object_kind": "merge_request",
+      "project": {
+        "name": "mr-test",
+        "web_url": "https://main.gitlab.in.here.com/stainsby/mr-test"
+      },
+      "user": {
+        "email": "hayden.stainsby@here.com",
+        "id": 1069,
+        "name": "Stainsby, Hayden",
+        "username": "stainsby"
+      }
+      }
+      "#;
+
+      let expected = Webhook::MergeRequest(MergeRequestWebhook {
+          assignees: None,
+          changes: None,
+          merge_request: MergeRequestAttributes {
+              iid: 3,
+              merge_status: MergeStatus::Unchecked,
+              title: "Fail pipeline".to_owned(),
+              url: "https://main.gitlab.in.here.com/stainsby/mr-test/-/merge_requests/3".to_owned(),
+          },
+          project: Project {
+              name: "mr-test".to_owned(),
+              web_url: "https://main.gitlab.in.here.com/stainsby/mr-test".to_owned(),
+          },
+          user: User {
+              email: "hayden.stainsby@here.com".to_owned(),
+              id: 1069,
+              name: "Stainsby, Hayden".to_owned(),
+              username: "stainsby".to_owned(),
+          },
+      });
+
+      println!("{}", json);
+      let webhook: Webhook = serde_json::from_str(&json).unwrap();
+      assert_eq!(expected, webhook);
+    }
+
+    #[test]
+    fn test_deserialize_pipeline() {
+        let json = r#"
+      {
+      "object_attributes": {
+        "finished_at": null,
+        "id": 4038106,
+        "ref": "fail-pipeline",
+        "status": "running"
+      },
+      "object_kind": "pipeline",
+      "project": {
+        "name": "mr-test",
+        "web_url": "https://main.gitlab.in.here.com/stainsby/mr-test"
+      },
+      "user": {
+        "email": "hayden.stainsby@here.com",
+        "id": 1069,
+        "name": "Stainsby, Hayden",
+        "username": "stainsby"
+      }
+      }
+      "#;
+
+      let expected = Webhook::Pipeline(PipelineWebhook {
+          pipeline: PipelineAttributes {
+              finished_at: None,
+              id: 4038106,
+              ref_: "fail-pipeline".to_owned(),
+              status: PipelineStatus::Running,
+          },
+          project: Project {
+              name: "mr-test".to_owned(),
+              web_url: "https://main.gitlab.in.here.com/stainsby/mr-test".to_owned(),
+          },
+          user: User {
+              email: "hayden.stainsby@here.com".to_owned(),
+              id: 1069,
+              name: "Stainsby, Hayden".to_owned(),
+              username: "stainsby".to_owned(),
+          },
+      });
+
+      println!("{}", json);
+      let webhook: Webhook = serde_json::from_str(&json).unwrap();
+      assert_eq!(expected, webhook);
+    }
+
 }
